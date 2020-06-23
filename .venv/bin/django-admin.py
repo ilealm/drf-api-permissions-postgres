214726fb6782188bbb95e184f39d032c66ba095f
Iris Leal <iris_leal_m@hotmail.com>
2020-06-23 12:34:27 -0700

    Initial setup

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ileal/codefellows/401/drf-api-permissions-postgres/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
