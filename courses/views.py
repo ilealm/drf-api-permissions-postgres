1b275fed1198bf2253a1f81bb8388e00fa3ddd28
Iris Leal <iris_leal_m@hotmail.com>
2020-06-23 13:29:59 -0700

    Now it renders and save with super user, not yet installed docker

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from .models import Course
+from .serializers import CourseSerializer
 
-# Create your views here.
+class CoursesList(ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class CoursesDetail(RetrieveUpdateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    
--- summer_camp_api_project/settings.py
+++ summer_camp_api_project/settings.py
@@ -122,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERSMISSION_CLASSES' : [
+            'rest_framework.permissions.AllowAny',
+    ]
+
+}
